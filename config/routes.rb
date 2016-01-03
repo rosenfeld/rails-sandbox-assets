a2c9b5f9f85aa108a776c457dc7cb4774f1af282
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2016-01-03 10:29:39 -0200

    Fix issue preventing Sidekiq from booting the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SandboxAssets::Engine.routes.draw do
-  mount Rails.application.assets => Rails.configuration.assets.prefix
+  # When the application is booted by sidekiq (tested with 4.0.1) Rails.application.assets may not
+  # be loaded
+  mount Rails.application.assets => Rails.configuration.assets.prefix if Rails.application.assets
   get '_iframe/*path' => 'sandbox_assets/test_runner#iframe', format: false,
     as: :sandbox_assets_iframe
   ['', '*path'].each do |p|
